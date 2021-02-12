14a675c21381e6c41856ab111d2b4f93a4af8a4a
Tomasz Nosewicz <tomasz.nosewicz@o2.pl>
2021-02-12 08:42:42 +0100

    Added 'given when then' labels to improve readability

--- src/test/java/com/noscompany/custom/linked/list/iterator/AddingElements.java
+++ src/test/java/com/noscompany/custom/linked/list/iterator/AddingElements.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.ListIterator;
 
 import static com.noscompany.custom.linked.list.iterator.IteratorAssertions.*;
+import static java.util.List.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddingElements {
@@ -15,17 +15,19 @@ public class AddingElements {
     @Test
     @DisplayName("adding on the beginning should succeed")
     public void test() {
-        List<String> objects = new ArrayList<>(List.of("a", "b", "c"));
-        ListIterator<String> iterator = objects.listIterator();
+//        given that there is iterator with elements "a", "b", "c"
+        ListIterator<String> iterator = new ArrayList<>(of("a", "b", "c")).listIterator();
+//        and iterator is at the beginning
         assertIsAtTheBeginning(iterator);
+//        when adding element "x"
         iterator.add("x");
-        assertEquals(0, iterator.previousIndex());
-        assertEquals(1, iterator.nextIndex());
-        assertEquals("x", iterator.previous());
-        assertIsAtTheBeginning(iterator);
-        iterateFromHeadToTailAndCheckStateEveryStep(iterator, "x", "a", "b", "c");
-        assertIsAtTheEnd(iterator);
-        iterateFromTailToHeadAndCheckStateEveryStep(iterator, "x", "a", "b", "c");
-        assertIsAtTheBeginning(iterator);
+//        then iterator is at position 1
+        assertPosition(iterator, 1);
+//        when moving one step back
+        String x = iterator.previous();
+//        then given element equals "x"
+        assertEquals("x", x);
+//        and moving from head to tail and back works correctly
+        moveFromHeadToTailAndBackAndCheckStateEveryStep(iterator, "x", "a", "b", "c");
     }
 }
